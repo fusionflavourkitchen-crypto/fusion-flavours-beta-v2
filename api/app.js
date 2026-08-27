f2e80ef05a0f3d755d2e967d295e76adb4b730d7
fusionflavourkitchen-crypto <fusionflavourkitchen@gmail.com>
2026-08-27 22:42:34 +0100

    Bump build after Service and P&L timer cleanup

--- api/app.js
+++ api/app.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 const path = require('path');
 
-const BUILD = '20260827-refactor-12';
+const BUILD = '20260827-refactor-13';
 
 module.exports = async function handler(req, res) {
   try {
