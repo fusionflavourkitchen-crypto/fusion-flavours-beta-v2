a41a722ad53cde1b7a41b099ea7a0d9b115bd045
fusionflavourkitchen-crypto <fusionflavourkitchen@gmail.com>
2026-08-28 17:03:13 +0100

    Bump build for main Delivery preorder removal

--- api/app.js
+++ api/app.js
@@ -2,7 +2,7 @@ const fs = require('fs');
 const path = require('path');
 const { migrateLegacyHtml, migrationFailures } = require('./lib/legacy-html-migration');
 
-const BUILD = '20260828-refactor-32';
+const BUILD = '20260828-refactor-33';
 
 module.exports = async function handler(req, res) {
   try {
