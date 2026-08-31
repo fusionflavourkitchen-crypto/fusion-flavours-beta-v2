546e50f59d034b96ac139e396755f4590148d6dc
fusionflavourkitchen-crypto <fusionflavourkitchen@gmail.com>
2026-08-31 12:47:28 +0100

    Force fresh Delivery note asset build

--- api/app.js
+++ api/app.js
@@ -2,7 +2,7 @@ const fs = require('fs');
 const path = require('path');
 const { migrateLegacyHtml, migrationFailures } = require('./lib/legacy-html-migration');
 
-const BUILD = '20260831-delivery-note-40';
+const BUILD = '20260831-delivery-note-41';
 
 module.exports = async function handler(req, res) {
   try {
