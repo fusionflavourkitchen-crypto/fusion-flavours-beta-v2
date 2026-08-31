39254062da49928bddfb189c43287e94ec1a68a0
fusionflavourkitchen-crypto <fusionflavourkitchen@gmail.com>
2026-08-31 11:08:46 +0100

    Restore last stable app build after blank-screen regression

--- api/app.js
+++ api/app.js
@@ -2,7 +2,7 @@ const fs = require('fs');
 const path = require('path');
 const { migrateLegacyHtml, migrationFailures } = require('./lib/legacy-html-migration');
 
-const BUILD = '20260828-refactor-35';
+const BUILD = '20260831-stable-rollback-39';
 
 module.exports = async function handler(req, res) {
   try {
