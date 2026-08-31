ff990ecae6858c6123cb2351ff47ff522a7b9093
fusionflavourkitchen-crypto <fusionflavourkitchen@gmail.com>
2026-08-31 11:36:54 +0100

    Bump delivery note asset version

--- api/app.js
+++ api/app.js
@@ -2,7 +2,7 @@ const fs = require('fs');
 const path = require('path');
 const { migrateLegacyHtml, migrationFailures } = require('./lib/legacy-html-migration');
 
-const BUILD = '20260831-stable-rollback-39';
+const BUILD = '20260831-delivery-note-40';
 
 module.exports = async function handler(req, res) {
   try {
